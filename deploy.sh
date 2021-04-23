108a5125827d7449faac520e3bb7aea739725e7c
Julie <julie.nanta@vop.mobi>
2021-04-23 14:06:04 +0000

    Change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 ssh root@139.162.235.6 <<EOF
  cd ~/T-WEB-800_2021
  git pull
- npm install — production
+ npm install
  tsc
  pm2 restart app
  exit
--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 const PORT = 4040;
 app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+    res.send('The sedulous hyena ate the antelope! JENKINS2');
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=app.js.map
