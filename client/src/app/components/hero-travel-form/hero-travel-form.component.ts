e2f81403de501b883d31627b7f441cf970148d31
ArnaudMichel <arnaud.michel@epitech.eu>
2021-07-02 16:14:50 +0200

    integration run, test e2e on front, reponsive design

--- client/src/app/components/hero-travel-form/hero-travel-form.component.ts
+++ client/src/app/components/hero-travel-form/hero-travel-form.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core'
   template: `
     <section class="hero-travel">
       <div class="img-travel"></div>
-      <div class="row align-center-center">
-        <div class="column-8 lg-offset-2 travel-form">
+      <div class="row">
+        <div class="sm-column-22 sm-offset-1 lg-column-8 lg-offset-2 travel-form">
           <form action="/">
-            <div class="row align-center-center">
-              <div class="column-18">
+            <div class="row">
+              <div class="sm-column-22 sm-offset-1 lg-column-22 lg-offset-1">
                 <label for="where">
                   <h4>Where whould you like to go ?</h4>
                 </label>
@@ -21,7 +21,7 @@ import { Component, OnInit } from '@angular/core'
                   placeholder="Destination: Country, City, Airport..."
                 />
               </div>
-              <div class="column-8">
+              <div class="sm-column-22 sm-offset-1 lg-column-10 lg-offset-1">
                 <input
                   class="a-input-text"
                   type="date"
@@ -29,15 +29,15 @@ import { Component, OnInit } from '@angular/core'
                   id="check-in"
                 />
               </div>
-              <div class="column-8 lg-offset-2">
+              <div class="sm-column-22 sm-offset-1 lg-column-10 lg-offset-1">
                 <input
                   class="a-input-text"
                   type="date"
                   name="check-out"
-                  id="check-in"
+                  id="check-out"
                 />
               </div>
-              <div class="column-8">
+              <div class="sm-column-22 sm-offset-1 lg-column-8 lg-offset-1">
                 <select class="a-select" name="adults" id="adults">
                   <option value="1" selected>
                     Adults
@@ -59,7 +59,7 @@ import { Component, OnInit } from '@angular/core'
                   </option>
                 </select>
               </div>
-              <div class="column-8 lg-offset-2">
+              <div class="sm-column-22 sm-offset-1 lg-column-13 lg-offset-1">
                 <select class="a-select" name="Max Budget" id="Max Budget">
                   <option value="1" selected>
                     Max Budget
@@ -81,13 +81,13 @@ import { Component, OnInit } from '@angular/core'
                   </option>
                 </select>
               </div>
-              <div class="column-18">
+              <div class="sm-column-22 sm-offset-1 lg-column-14 lg-offset-5">
                 <a href="" class="a-button secondary">Book Now</a>
               </div>
             </div>
           </form>
         </div>
-        <div class="column-8 lg-offset-2">
+        <div class="sm-column-22 sm-offset-1 lg-column-8 lg-offset-2 pt-30">
           <div class="row">
             <div class="column-24">
               <h2>Let's discover the world together</h2>
